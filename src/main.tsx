cbc06ea53a2bd61ad8d381e37339119e269a716a
Senka Petrovic <senka.sp.petrovic@example.com>
2024-03-04 09:42:54 -0800

    RMR-1 Improved translations

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import './locales/translation.ts';
+import './locales/en/en.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
