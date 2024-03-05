652f0e909a347ce2a439bf7018d28d004bc7216c
Senka Petrovic <senka.sp.petrovic@example.com>
2024-03-05 12:56:59 -0800

    RMR-2 Styled CharacterList

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -1,14 +1,15 @@
-import axios from 'axios';
-import { useQuery } from 'react-query';
+import axios from "axios";
+import { useQuery } from "react-query";
+import styles from "./CharacterList.module.scss";
 
 //TO UPDATE?
 interface Character {
-    id: number;
-    name: string;
-    status: string;
-    species: string;
-    gender: string;
-    image: string;
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  gender: string;
+  image: string;
 }
 
 const getCharacters = async () => {
@@ -19,18 +20,16 @@ const getCharacters = async () => {
 };
 
 const CharacterList = () => {
-
-  const { data, isLoading, isError } = useQuery('characters', getCharacters);
+  const { data, isLoading, isError } = useQuery("characters", getCharacters);
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error fetching data</div>;
 
   return (
-    <div>
-      <h2>Character List</h2>
+    <div className={styles.characters}>
       {data?.map((character: Character) => (
-        <div key={character?.id}>
-            <img src={character?.image} alt={character?.name} />
+        <div key={character?.id} className={styles.characterItem}>
+          <img src={character?.image} alt={character?.name} />
         </div>
       ))}
     </div>
