8f0fa761dc744bce76bc0b5097825f3d63242fe3
Senka Petrovic <senka.sp.petrovic@example.com>
2024-03-05 15:40:03 -0800

    Created pagination

--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { useQuery } from "react-query";
+import { Pagination } from "antd";
 import styles from "./CharacterList.module.scss";
+import { useState } from "react";
 
-//TO UPDATE?
+// TO UPDATE?
 interface Character {
   id: number;
   name: string;
@@ -12,27 +14,50 @@ interface Character {
   image: string;
 }
 
-const getCharacters = async () => {
-  let apiUrl = "https://rickandmortyapi.com/api/character";
+const PER_PAGE = 30;
+
+const getCharacters = async (page: number) => {
+  let apiUrl = `https://rickandmortyapi.com/api/character?page=${page}`;
   const response = await axios.get(apiUrl);
 
-  return response?.data?.results;
+  return response?.data;
 };
 
 const CharacterList = () => {
-  const { data, isLoading, isError } = useQuery("characters", getCharacters);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data, isLoading, isError } = useQuery(
+    ["characters", currentPage],
+    () => getCharacters(currentPage)
+  );
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error fetching data</div>;
 
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const pageCount = data.info.pages;
+
   return (
-    <div className={styles.characters}>
-      {data?.map((character: Character) => (
-        <div key={character?.id} className={styles.characterItem}>
-          <img src={character?.image} alt={character?.name} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className={styles.pagination}>
+        <Pagination
+          current={currentPage}
+          total={pageCount * PER_PAGE}
+          pageSize={PER_PAGE}
+          onChange={handlePageChange}
+          showSizeChanger={false}
+        />
+      </div>
+      <div className={styles.characters}>
+        {data?.results?.map((character: Character) => (
+          <div key={character?.id} className={styles.characterItem}>
+            <img src={character?.image} alt={character?.name} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
