6406cae3b5d529341a82ec5aaabaec9acdc57f5a
Senka Petrovic <senka.sp.petrovic@example.com>
2024-03-13 12:15:11 -0700

    RMR-4 Added translations

--- src/components/CharacterList/CharacterDetails/CharacterDetails.tsx
+++ src/components/CharacterList/CharacterDetails/CharacterDetails.tsx
@@ -1,6 +1,7 @@
 import { Modal } from "antd"
 import Character from "../Character.interface";
 import styles from "./CharacterDetails.module.scss";
+import { useTranslation } from "react-i18next";
 
 interface CharacterDetailsProps {
     selectedCharacter: Character | null;
@@ -8,27 +9,28 @@ interface CharacterDetailsProps {
 }
 
 const CharacterDetails: React.FC<CharacterDetailsProps> = ({ selectedCharacter, closeModal }) => {
-  
-    return (
-      <Modal
-        open={!!selectedCharacter}
-        title={selectedCharacter?.name}
-        onCancel={closeModal}
-        footer={null}
-        centered
-        className={styles.modelStyle}
-      >
-        {selectedCharacter && (
-          <>
-            <img src={selectedCharacter.image} alt={selectedCharacter.name} />
-            <p>Name: <b>{selectedCharacter.name}</b></p>
-            <p>Gender: <b>{selectedCharacter.gender}</b></p>
-            <p>Species: <b>{selectedCharacter.species}</b></p>
-            <p>Location Name: <b>{selectedCharacter.location.name}</b></p>
-          </>
-        )}
-      </Modal>
-    );
+  const { t } = useTranslation();
+
+  return (
+    <Modal
+      open={!!selectedCharacter}
+      title={selectedCharacter?.name}
+      onCancel={closeModal}
+      footer={null}
+      centered
+      className={styles.modelStyle}
+    >
+      {selectedCharacter && (
+        <>
+          <img src={selectedCharacter.image} alt={selectedCharacter.name} />
+          <p>{t('name')}: <b>{selectedCharacter.name}</b></p>
+          <p>{t('gender')}: <b>{selectedCharacter.gender}</b></p>
+          <p>{t('species')}: <b>{selectedCharacter.species}</b></p>
+          <p>{t('locationName')}: <b>{selectedCharacter.location.name}</b></p>
+        </>
+      )}
+    </Modal>
+  );
 };
 
 export default CharacterDetails;
