b6d5d0d95daeab1fac4ac2517395d3bce392da99
Senka Petrovic <petrovicsenka@outlook.com>
2024-04-16 14:49:34 -0700

    RMR-5 Added icon and updated CSS

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
-import { Button, Input } from "antd";
+import { Button, Input } from 'antd';
 import { useNavigate } from 'react-router-dom';
-import { useContext } from "react";
-import { useTranslation } from "react-i18next";
-import styles from "./Header.module.scss";
-import { CharacterDataContext } from "../../contexts/CharacterDataContext/CharacterDataContext";
+import { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import styles from './Header.module.scss';
+import { CharacterDataContext } from '../../contexts/CharacterDataContext/CharacterDataContext';
+import logo from '../../assets/rick-and-morty-icon.png';
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
@@ -21,24 +22,33 @@ const Header: React.FC = () => {
 
   return (
     <nav className={styles.nav}>
+      <img src={logo} alt={'Rick and Morty'} className={styles.logo} />
       <span className={styles.title}>{t('characterListTitle')}</span>
       <div className={styles.controls}>
         <Input
-          type="text"
+          type='text'
           placeholder={t('search')}
           value={searchFilter as string}
           onChange={(e) => handleUpdateFilter('search', e.target.value)}
           className={`${styles.headerElement} ${styles.inputElement}`}
         />
-        <Button type="default" className={styles.headerElement} onClick={navigateToFavourites}>
+        <Button
+          type='default'
+          className={styles.headerElement}
+          onClick={navigateToFavourites}
+        >
           {t('favourites')}
         </Button>
-        <Button type="default" className={styles.headerElement} onClick={logout}>
+        <Button
+          type='default'
+          className={styles.headerElement}
+          onClick={logout}
+        >
           {t('logout')}
         </Button>
       </div>
     </nav>
   );
-}
+};
 
 export default Header;
